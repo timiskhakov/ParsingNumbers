66c44689c4a9e7543c17cf6023bcabc1f8f2856a
Timur Iskhakov <timiskhakov@gmail.com>
2022-09-10 20:15:19 +0300

    Format bytes

--- ParsingNumbers/Parsers/Block.cs
+++ ParsingNumbers/Parsers/Block.cs
@@ -31,8 +31,11 @@ public class Block
             padded.CopyTo(bytes, i * NumberSize);
         }
 
-        Mask = Vector128.Create(bytes[0], bytes[1], bytes[2], bytes[3], bytes[4], bytes[5], bytes[6], bytes[7],
-            bytes[8], bytes[9], bytes[10], bytes[11], bytes[12], bytes[13], bytes[14], bytes[15]);
+        Mask = Vector128.Create(
+            bytes[0], bytes[1], bytes[2], bytes[3],
+            bytes[4], bytes[5], bytes[6], bytes[7],
+            bytes[8], bytes[9], bytes[10], bytes[11],
+            bytes[12], bytes[13], bytes[14], bytes[15]);
     }
 
     private static (List<Pattern>, int) FindPatterns(ushort value)
