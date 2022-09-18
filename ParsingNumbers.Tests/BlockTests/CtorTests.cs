47351c0220b472435f8c26152ae32baca50cb09f
Timur Iskhakov <timiskhakov@gmail.com>
2022-09-18 08:21:04 +0300

    Add more test asserts for block tests

--- ParsingNumbers.Tests/BlockTests/CtorTests.cs
+++ ParsingNumbers.Tests/BlockTests/CtorTests.cs
@@ -1,4 +1,5 @@
 ﻿using ParsingNumbers.Parsers;
+using System.Runtime.Intrinsics;
 using Xunit;
 
 namespace ParsingNumbers.Tests.BlockTests;
@@ -12,6 +13,8 @@ public class CtorTests
 
         Assert.Equal(1, block.NumberSize);
         Assert.Equal(7, block.Amount);
+        Assert.Equal(15, block.Processed);
+        Assert.Equal(Vector128.Create((byte)1, 3, 5, 7, 9, 11, 13, 0, 0, 0, 0, 0, 0, 0, 0, 0), block.Mask);
     }
 
     [Fact]
@@ -22,6 +25,7 @@ public class CtorTests
         Assert.Equal(1, block.NumberSize);
         Assert.Equal(8, block.Amount);
         Assert.Equal(16, block.Processed);
+        Assert.Equal(Vector128.Create((byte)0, 2, 4, 6, 8, 10, 12, 14, 0, 0, 0, 0, 0, 0, 0, 0), block.Mask);
     }
 
     [Fact]
@@ -31,6 +35,8 @@ public class CtorTests
 
         Assert.Equal(8, block.NumberSize);
         Assert.Equal(2, block.Amount);
+        Assert.Equal(16, block.Processed);
+        Assert.Equal(Vector128.Create(128, 128, 128, 1, 2, 3, 4, 5, 128, 128, 128, 128, 128, 128, 128, 7), block.Mask);
     }
 
     [Fact]
@@ -40,5 +46,7 @@ public class CtorTests
 
         Assert.Equal(4, block.NumberSize);
         Assert.Equal(4, block.Amount);
+        Assert.Equal(16, block.Processed);
+        Assert.Equal(Vector128.Create(128, 1, 2, 3, 128, 128, 128, 6, 128, 9, 10, 11, 128, 128, 13, 14), block.Mask);
     }
 }
